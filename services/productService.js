3340f208def7e2d8cda7389354aa2821c867aa5c
JinLiu <jinliu214@gmail.com>
2026-05-09 23:04:40 -0700

    final commit

--- services/productService.js
+++ services/productService.js
@@ -2,8 +2,12 @@
 // 產品服務
 // ========================================
 
-const { fetchProducts } = require('../api');
-const { getDiscountRate, getAllCategories, formatCurrency } = require('../utils');
+const { fetchProducts } = require("../api");
+const {
+  getDiscountRate,
+  getAllCategories,
+  formatCurrency,
+} = require("../utils");
 
 /**
  * 取得所有產品
@@ -13,6 +17,11 @@ async function getProducts() {
   // 請實作此函式
   // 提示：使用 fetchProducts() 取得產品陣列
   // 回傳格式：{ products, count: 產品數量 }
+  const products = await fetchProducts();
+  return {
+    products,
+    count: products.length,
+  };
 }
 
 /**
@@ -24,6 +33,8 @@ async function getProductsByCategory(category) {
   // 請實作此函式
   // 提示：使用 fetchProducts() 取得所有產品後，篩選出符合 category 的產品
   // 回傳格式：篩選後的產品陣列
+  const products = await fetchProducts();
+  return products.filter((product) => product.category === category);
 }
 
 /**
@@ -35,6 +46,8 @@ async function getProductById(productId) {
   // 請實作此函式
   // 提示：使用 fetchProducts() 取得所有產品後，找出 id 符合的產品
   // 若找不到，回傳 null
+  const products = await fetchProducts();
+  return products.find((product) => product.id === productId) || null;
 }
 
 /**
@@ -44,6 +57,8 @@ async function getProductById(productId) {
 async function getCategories() {
   // 請實作此函式
   // 提示：使用 fetchProducts() 取得所有產品後，代入到 utils getAllCategories()
+  const products = await fetchProducts();
+  return getAllCategories(products);
 }
 
 /**
@@ -63,6 +78,15 @@ function displayProducts(products) {
   //    原價：NT$ 1,000
   //    售價：NT$ 800 (8折)
   // ----------------------------------------
+  products.forEach((product, index) => {
+    console.log(`${index + 1}. ${product.title}`);
+    console.log(`   分類：${product.category}`);
+    console.log(`   原價：${formatCurrency(product.origin_price)}`);
+    console.log(
+      `   售價：${formatCurrency(product.price)} (${getDiscountRate(product)})`,
+    );
+    console.log("----------------------------------------");
+  });
 }
 
 module.exports = {
@@ -70,5 +94,5 @@ module.exports = {
   getProductsByCategory,
   getProductById,
   getCategories,
-  displayProducts
+  displayProducts,
 };
